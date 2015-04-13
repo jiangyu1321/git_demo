427f16fe7e0249ec5784695f7f39c3cdd432b22c
jiangyu1321 <704351844@qq.com>
2015-04-14 00:27:44 +0800

    ff

--- src/com/jjyy/bean/User.java
+++ src/com/jjyy/bean/User.java
@@ -1,5 +1,15 @@
 package com.jjyy.bean;
 
 public class User {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
